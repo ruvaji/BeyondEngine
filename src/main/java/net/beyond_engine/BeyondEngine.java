55d06cbadef845c49d9b1c66c43c06d749c9862c
Ruvaji <ruvaji81@gmail.com>
2025-04-06 01:35:12 +0700

    Initial commit

---
+++ src/main/java/net/beyond_engine/BeyondEngine.java
@@ -0,0 +1,52 @@
+package net.beyond_engine;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(BeyondEngine.MOD_ID)
+public class BeyondEngine
+{
+
+    public static final String MOD_ID = "beyond_engine";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public BeyondEngine(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::AddCreative);
+
+    }
+
+    private void AddCreative(BuildCreativeModeTabContentsEvent event) {
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
